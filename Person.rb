def47a638c17cae24c8d475894ba2f072b871735
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-01-03 19:53:11 +0530

    Add all files and folders

---
+++ Person.rb
@@ -0,0 +1,28 @@
+class Person
+  attr_reader :id
+  attr_accessor :name, :age
+
+  def initialize (name = "Unknown", age, parent_permission: true)
+    @id = rand(1..10000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+  end
+  
+  def can_use_services?
+    is_of_age || @parent_permission
+  end  
+
+  private
+
+  def is_of_age?
+    @age >= 18
+  end    
+
+end
+
+  
+ 
+
+
+end      
