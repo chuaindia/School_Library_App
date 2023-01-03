f66827d5bc6e08c6a7c7c9208fab448c041e6eda
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-01-03 20:35:34 +0530

    correct Linters

--- Person.rb
+++ Person.rb
@@ -2,21 +2,20 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize (age, name = "Unknown", parent_permission: true)
-    @id = rand(1..10000)
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
   
   def can_use_services?
-    is_of_age || @parent_permission
-  end  
-
+    of_age || @parent_permission
+  end
+  
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
-  end    
-
+  end
 end
