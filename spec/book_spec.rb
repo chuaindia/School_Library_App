62522bd40f81d137a574dde30881330a5ec82343
Tanusri Ghosh <106825373+chuaindia@users.noreply.github.com>
2023-01-11 18:03:14 +0530

    Add book_spec file

---
+++ spec/book_spec.rb
@@ -0,0 +1,29 @@
+require_relative '../book'
+
+describe Book do
+
+  before :each do  
+  @book = Book.new('Maa', 'Maxim Gorky')
+  end
+
+  describe '#new' do
+    it 'retuns a new book object' do
+      @book.should be_an_instance_of Book
+    end
+  end  
+  
+  describe '#title' do
+    it 'returns the correct title' do
+      @book.title.should eql 'Maa'
+    end
+  end  
+  
+  bbbbb
+
+  describe '#author' do
+    it 'returns the correct author' do
+      @book.title.should eql 'Maxim Gorky'
+    end
+  end
+end     
+  
