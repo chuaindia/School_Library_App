2700ea9fcd9eb626f706aa4d58544fb18c4bcff3
Olivier-Kango <olivierkango@gmail.com>
2023-01-11 19:57:18 -0800

    Add test to base decorator

--- spec/base_decorator_spec.rb
+++ spec/base_decorator_spec.rb
@@ -5,7 +5,7 @@ describe BaseDecorator do
     before :each do
       @base_decorator = BaseDecorator.new('Dennis')
     end
-    
+
     it 'A new Base decorator is created' do
       expect(@base_decorator).to be_instance_of BaseDecorator
     end
